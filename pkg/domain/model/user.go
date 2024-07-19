bc33646776932dfeaf47ca070e5c6d06865cab4b
shou1027 <longword871@gmail.com>
2024-07-19 23:44:39 +0900

    ユーザーバリデーション実装

--- pkg/domain/model/user.go
+++ pkg/domain/model/user.go
@@ -1,8 +1,37 @@
 package model
 
+import "unicode/utf8"
+
+const (
+	nameLengthMax = 255
+	nameLengthMin = 1
+)
+
 type User struct {
-	ID       int64
-	Username string
-	Email    string
-	Password string
+	id       int64
+	name     string
+	email    string
+	password string
+}
+
+func NewUser(
+	id int64,
+	name string,
+	email string,
+) (*User, error) {
+	// 名前バリデーション
+	if utf8.RuneCountInString(name) < nameLengthMin || utf8.RuneCountInString(name) > nameLengthMax {
+		// return nil, errDomain.NewError("名前の文字数が不正です。")
+	}
+
+	// メールアドレスバリデーション
+	// if _, err := mail.ParseAddress(email); err != nil {
+	// 	return nil, errDomain.NewError("メールアドレスが不正です。")
+	// }
+
+	return &User{
+		id:    id,
+		name:  name,
+		email: email,
+	}, nil
 }
